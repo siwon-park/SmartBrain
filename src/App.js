57e1675a44a00c9fcc68e0e5acdfcb68c877ab91
siwonpark <siwonsiwon80@hotmail.com>
2019-09-26 15:29:13 -0400

    fixed the fetch address to heroku

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+    fetch('https://dry-woodland-52896.herokuapp.com/imageurl', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -104,7 +104,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://dry-woodland-52896.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://dry-woodland-52896.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'}, 
             body: JSON.stringify({

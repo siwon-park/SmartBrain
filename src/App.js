4d3d237b17c374d6f24b20831d62dc750452d5e6
siwonpark <siwonsiwon80@hotmail.com>
2019-09-26 15:47:12 -0400

    fixed port error

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://dry-woodland-52896.herokuapp.com:3000/image', {
+          fetch('https://dry-woodland-52896.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
